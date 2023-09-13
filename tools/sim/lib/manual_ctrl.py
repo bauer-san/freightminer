d054d5ac919ac447151de3bce2eb5cadb2fae2b2
Geoff Bauer <geoffbauer@gmail.com>
2023-09-12 21:25:00 -0400

    improve joystick interface to Carla

--- tools/sim/lib/manual_ctrl.py
+++ tools/sim/lib/manual_ctrl.py
@@ -18,19 +18,19 @@ button_states = {}
 
 # These constants were borrowed from linux/input.h
 axis_names = {
-  0x00 : 'x',
-  0x01 : 'y',
+  0x00 : 'x', # Logitech Extreme 3D pro
+  0x01 : 'y', # Logitech Extreme 3D pro
   0x02 : 'z',
   0x03 : 'rx',
   0x04 : 'ry',
-  0x05 : 'rz',
-  0x06 : 'trottle',
+  0x05 : 'rz', # Logitech Extreme 3D pro
+  0x06 : 'throttle', # Logitech Extreme 3D pro
   0x07 : 'rudder',
   0x08 : 'wheel',
   0x09 : 'gas',
   0x0a : 'brake',
-  0x10 : 'hat0x',
-  0x11 : 'hat0y',
+  0x10 : 'hat0x', # Logitech Extreme 3D pro
+  0x11 : 'hat0y', # Logitech Extreme 3D pro
   0x12 : 'hat1x',
   0x13 : 'hat1y',
   0x14 : 'hat2x',
@@ -47,18 +47,18 @@ axis_names = {
 }
 
 button_names = {
-  0x120 : 'trigger',
-  0x121 : 'thumb',
-  0x122 : 'thumb2',
-  0x123 : 'top',
-  0x124 : 'top2',
-  0x125 : 'pinkie',
-  0x126 : 'base',
-  0x127 : 'base2',
-  0x128 : 'base3',
-  0x129 : 'base4',
-  0x12a : 'base5',
-  0x12b : 'base6',
+  0x120 : 'trigger', # Logitech Extreme 3D pro
+  0x121 : 'thumb', # Logitech Extreme 3D pro
+  0x122 : 'thumb2', # Logitech Extreme 3D pro
+  0x123 : 'top', # Logitech Extreme 3D pro
+  0x124 : 'top2', # Logitech Extreme 3D pro
+  0x125 : 'pinkie', # Logitech Extreme 3D pro
+  0x126 : 'base', # Logitech Extreme 3D pro
+  0x127 : 'base2', # Logitech Extreme 3D pro
+  0x128 : 'base3', # Logitech Extreme 3D pro
+  0x129 : 'base4', # Logitech Extreme 3D pro
+  0x12a : 'base5', # Logitech Extreme 3D pro
+  0x12b : 'base6', # Logitech Extreme 3D pro
   0x12f : 'dead',
   0x130 : 'a',
   0x131 : 'b',
@@ -141,48 +141,75 @@ def wheel_poll_thread(q: 'Queue[str]') -> NoReturn:
   evtdev = InputDevice(device)
   val = 24000
   evtdev.write(ecodes.EV_FF, ecodes.FF_AUTOCENTER, val)
-
+#  print("queue size %d" % q.qsize())
+  
   while True:
+#    print("queue size %d" % q.qsize())
     evbuf = jsdev.read(8)
     value, mtype, number = struct.unpack('4xhBB', evbuf)
     # print(mtype, number, value)
     if mtype & 0x02:  # wheel & paddles
       axis = axis_map[number]
+      #Set vehicle control according to https://carla.readthedocs.io/en/latest/python_api/#instance-variables_73
+      if axis == "y":  # throttle
+        if value < 0:
+            fvalue = 30.0
+            if q.qsize() < 5:
+              q.put(f"throttle_{fvalue:f}")
 
-      if axis == "z":  # gas
-        fvalue = value / 32767.0
-        axis_states[axis] = fvalue
-        normalized = (1 - fvalue) * 50
-        q.put(f"throttle_{normalized:f}")
+      if axis == "x":  # steer angle
+        fvalue = 0.0
+        if value < -1:
+          fvalue = 0.07
+        if value > 1:
+          fvalue = -0.07
+        if q.qsize() < 5:
+            q.put(f"steer_{fvalue:f}")        
 
-      elif axis == "rz":  # brake
-        fvalue = value / 32767.0
-        axis_states[axis] = fvalue
-        normalized = (1 - fvalue) * 50
-        q.put(f"brake_{normalized:f}")
+#        if value < -1.: #left
+#            q.put(f"steer_{fvalue:f}")
+#        elif value > +1: #right
+#            q.put(f"steer_{fvalue:f}")
 
-      elif axis == "x":  # steer angle
-        fvalue = value / 32767.0
-        axis_states[axis] = fvalue
-        normalized = fvalue
-        q.put(f"steer_{normalized:f}")
+        #axis_states[axis] = fvalue
+        #normalized = fvalue
+        #q.put(f"steer_{normalized:f}")
+
+      if axis == "throttle":
+        if value > 0:
+            fvalue = 10.0
+            if q.qsize() < 5:
+              q.put(f"brake_{fvalue:f}")
+	
 
     elif mtype & 0x01:  # buttons
       if value == 1: # press down
-        if number in [0, 19]:  # X
+        if number in [0, 1]: #[0, 19]:  # trigger
+          #fvalue = 1.0
+          #q.put(f"throttle_{fvalue:f}")
+          #print("throttle button %d" % number)
+          q.put("brake_%f" % 1.0)
+          print("brake button %d" % number)
+          
+        elif number in [2]: #[0, 19]:  # X
           q.put("cruise_down")
 
-        elif number in [3, 18]:  # triangle
+        elif number in [4]: #[3, 18]:  # triangle
           q.put("cruise_up")
 
-        elif number in [1, 6]:  # square
+        elif number in [5]: #[1, 6]:  # square
           q.put("cruise_cancel")
 
-        elif number in [10, 21]:  # R3
+        elif number in [6]: #[10, 21]:  # R3
           q.put("reverse_switch")
+         
+        else:
+            print("Detected unused button number: %2d" % number)
 
 if __name__ == '__main__':
   from multiprocessing import Process, Queue
   q: Queue[str] = Queue()
   p = Process(target=wheel_poll_thread, args=(q,))
   p.start()
+
+
