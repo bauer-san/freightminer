ac9f98bd81352657eb003cb6471be8c48d6d3e02
bauer_san <geoffbauer@gmail.com>
2024-03-13 21:13:29 -0400

    Add files via upload

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -17,7 +17,7 @@
 #include "common/timing.h"
 #include "system/hardware/hw.h"
 
-const int UI_BORDER_SIZE = 30;
+const int UI_BORDER_SIZE = 0;
 const int UI_HEADER_HEIGHT = 420;
 
 const int UI_FREQ = 20; // Hz
